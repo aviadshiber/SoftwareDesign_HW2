9f1d2ccb37da5e56ea278421a8482cf83dc40dfc
aviadshiber <aviadshiber@campus.technion.ac.il>
2019-06-08 18:49:55 +0300

    adding local date time convertsion to utils

--- base/library/src/main/kotlin/il/ac/technion/cs/softwaredesign/storage/utils/ConversionUtils.kt
+++ base/library/src/main/kotlin/il/ac/technion/cs/softwaredesign/storage/utils/ConversionUtils.kt
@@ -1,10 +1,25 @@
 package il.ac.technion.cs.softwaredesign.storage.utils
 
 import com.google.common.primitives.Longs
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+
 
 object ConversionUtils {
 
     fun longToBytes(x: Long): ByteArray = Longs.toByteArray(x)
 
     fun bytesToLong(bytes: ByteArray): Long = Longs.fromByteArray(bytes)
+
+    fun localDateTimeToBytes(localDateTime: LocalDateTime):ByteArray{
+        val formatter = DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm:ss:SSS")
+        return localDateTime.format(formatter).toByteArray()
+    }
+
+    fun ByteArrayToLocalDateTime(bytes: ByteArray):LocalDateTime{
+        val formatter = DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm:ss:SSS")
+        return LocalDateTime.parse(String(bytes), formatter)
+    }
+
 }
