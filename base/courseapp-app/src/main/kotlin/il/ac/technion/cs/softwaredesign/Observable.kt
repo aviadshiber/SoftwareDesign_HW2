b5224e2b0b635b7983face7cc594fe8ea441989d
aviadshiber <aviadshiber@campus.technion.ac.il>
2019-06-10 20:22:45 +0300

    implementing observable

--- base/courseapp-app/src/main/kotlin/il/ac/technion/cs/softwaredesign/Observable.kt
+++
@@ -1,8 +0,0 @@
-package il.ac.technion.cs.softwaredesign
-
-import java.util.concurrent.CompletableFuture
-
-interface Observable<S, M> {
-    fun listen(listener: (S, M) -> CompletableFuture<Unit>)
-    fun unlisten(listener: (S, M) -> CompletableFuture<Unit>)
-}
