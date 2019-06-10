8e4fcaedf72eedb9e3c476da3875f34e990111c3
aviadshiber <aviadshiber@campus.technion.ac.il>
2019-06-10 22:11:24 +0300

    adding test Broadcast received only after users listen

--- base/courseapp-app/src/test/kotlin/il/ac/technion/cs/softwaredesign/tests/CourseAppTestHw2.kt
+++ base/courseapp-app/src/test/kotlin/il/ac/technion/cs/softwaredesign/tests/CourseAppTestHw2.kt
@@ -1,16 +1,24 @@
 import com.authzee.kotlinguice4.getInstance
 import com.google.inject.Guice
 import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
 import com.natpryce.hamkrest.present
-import il.ac.technion.cs.softwaredesign.CourseApp
-import il.ac.technion.cs.softwaredesign.CourseAppInitializer
-import il.ac.technion.cs.softwaredesign.CourseAppStatistics
+import il.ac.technion.cs.softwaredesign.*
+import il.ac.technion.cs.softwaredesign.messages.MediaType
+import il.ac.technion.cs.softwaredesign.messages.MessageFactory
+import il.ac.technion.cs.softwaredesign.storage.SecureStorageModule
 import il.ac.technion.cs.softwaredesign.tests.CourseAppTestModule
 import il.ac.technion.cs.softwaredesign.tests.isTrue
 import il.ac.technion.cs.softwaredesign.tests.runWithTimeout
+import io.github.vjames19.futures.jdk8.ImmediateFuture
+import io.mockk.confirmVerified
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import java.time.Duration
+import java.util.concurrent.CompletableFuture
 
 @TestInstance(TestInstance.Lifecycle.PER_METHOD)
 class CourseAppTestHw2 {
@@ -18,9 +26,37 @@ class CourseAppTestHw2 {
     private val courseApp = injector.getInstance<CourseApp>()
     private val courseAppStatistics = injector.getInstance<CourseAppStatistics>()
     private val courseAppInitializer = injector.getInstance<CourseAppInitializer>()
+    private val messageFactory = injector.getInstance<MessageFactory>()
+
 
     init {
-        courseAppInitializer.setup()
+        injector.getInstance<CourseAppInitializer>().setup().join()
+    }
+
+    @Test
+    fun `Broadcast received only after users listen`() {
+        var ronNoCalls=0
+        var aviadNoCall=0
+        val ronCallback :ListenerCallback = {
+            source, message -> ImmediateFuture { ronNoCalls++; println("sent from $source"+String(message.contents))}
+        }
+        val aviadCallback :ListenerCallback = {
+            source, message -> ImmediateFuture { aviadNoCall++; println("sent from $source"+String(message.contents))}
+        }
+
+        val aviad=courseApp.login("aviad","shiber").join()
+        val ron=courseApp.login("ron","123").join()
+        messageFactory.create(MediaType.TEXT, "no one should receive it".toByteArray())
+                .thenCompose {courseApp.broadcast(aviad,it)  }
+                .thenCompose {  courseApp.addListener(ron,ronCallback) }
+                .thenCompose { courseApp.addListener(aviad,aviadCallback) }
+                .thenCompose { messageFactory.create(MediaType.TEXT, "aviad and ron should get this".toByteArray())  }
+                .thenCompose { courseApp.broadcast(aviad,it) }.join()
+
+
+        assertThat( ronNoCalls , equalTo(1))
+        assertThat( aviadNoCall , equalTo(1))
+
     }
 
     @Test
