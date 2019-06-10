1f8c277a877a7f3f2390062310a0d50595507a16
RonYitzhak <ron_itz@campus.technion.ac.il>
2019-06-10 20:44:19 +0300

    implement new statistics

--- base/courseapp-app/src/main/kotlin/il/ac/technion/cs/softwaredesign/CourseAppStatisticsImpl.kt
+++ base/courseapp-app/src/main/kotlin/il/ac/technion/cs/softwaredesign/CourseAppStatisticsImpl.kt
@@ -1,22 +1,25 @@
 package il.ac.technion.cs.softwaredesign
 
+import il.ac.technion.cs.softwaredesign.managers.MessageManager
 import il.ac.technion.cs.softwaredesign.storage.api.IChannelManager
+import il.ac.technion.cs.softwaredesign.storage.api.IMessageManager
 import il.ac.technion.cs.softwaredesign.storage.api.IUserManager
 import java.util.concurrent.CompletableFuture
 import javax.inject.Inject
 
 class CourseAppStatisticsImpl @Inject constructor(private val userManager: IUserManager,
+                                                  private val messageManager: IMessageManager,
                                                   private val channelManager: IChannelManager) : CourseAppStatistics {
     override fun pendingMessages(): CompletableFuture<Long> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return messageManager.getNumberOfPendingMessages()
     }
 
     override fun channelMessages(): CompletableFuture<Long> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return channelManager.getNumberOfTotalChannelMessages()
     }
 
     override fun top10ChannelsByMessages(): CompletableFuture<List<String>> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return channelManager.getTop10ChannelsByMsgsCount()
     }
 
     override fun totalUsers(): CompletableFuture<Long> {
